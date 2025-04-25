b83315fd2afe40f07116c16eb4e98e09f5ac369a
DenisBG312 <bgdenibg@gmail.com>
2025-04-25 17:48:59 +0300

    Add project files.

---
+++ FlatRockTechnologyTask/Models/ProductInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace FlatRockTechnologyTask.Models
+{
+    public class ProductInfo
+    {
+
+        public ProductInfo(string productName, string price, string rating)
+        {
+            Name = productName;
+            Price = price;
+            Rating = rating;
+        }
+        [JsonPropertyName("productName")]
+        public string Name { get; set; }
+        [JsonPropertyName("price")]
+        public string Price { get; set; }
+        [JsonPropertyName("rating")]
+        public string Rating { get; set; }
+    }
+}
---
+++ FlatRockTechnologyTask/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Web;
+using FlatRockTechnologyTask.Models;
+using HtmlAgilityPack;
+
+namespace FlatRockTechnologyTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var html = File.ReadAllText("index.html");
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            var elements = doc.DocumentNode.SelectNodes("//div[@class='item']");
+
+            var products = new List<ProductInfo>();
+
+            foreach (var element in elements)
+            {
+                var imgNode = element.SelectSingleNode(".//img");
+                var productName = HttpUtility.HtmlDecode(imgNode?.GetAttributeValue("alt", "").Trim());
+
+                var priceNode = element.SelectSingleNode(".//span[@class='price-display formatted']");
+                var priceText = priceNode?.SelectSingleNode(".//span[@style='display: none']")?.InnerText;
+
+                var price = priceText?.Substring(1).Replace(",", "");
+
+                var ratingAttr = element.GetAttributeValue("rating", "0");
+                var rating = double.Parse(ratingAttr);
+
+                if (rating > 5)
+                {
+                    rating /= 2;
+                }
+
+                var ratingStr = rating.ToString();
+
+                var productInfo = new ProductInfo(productName, price, ratingStr);
+
+
+                products.Add(productInfo);
+            }
+
+            var jsonOutput = JsonSerializer.Serialize(products, new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            });
+
+            Console.WriteLine(jsonOutput);
+        }
+    }
+}
